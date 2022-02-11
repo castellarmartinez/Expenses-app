50e580f22a245d3d30fdae4566081eed75acaa58
David Castellar Martínez <castellardav@gmail.com>
2022-02-11 12:05:13 -0500

    Add first event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,14 +2,19 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card.js';
 
-const ExpenseItem = (props) => {    
+const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('Clicked!!!!');
+    };
+
     return (
         <Card className="expense-item">
-            <ExpenseDate date = {props.date}/>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">$ {props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>);
 }
 
