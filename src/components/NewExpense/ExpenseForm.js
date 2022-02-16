d5df29fe9c580533528b52713e6c3c1a99a81b6b
David Castellar Martínez <castellardav@gmail.com>
2022-02-16 12:54:23 -0500

    Reset form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -28,6 +28,10 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData);
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
@@ -35,16 +39,23 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input 
+                        type='text' 
+                        value={enteredTitle}
+                        onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01"
+                    <input 
+                        type='number' min="0.01" step="0.01"
+                        value={enteredAmount}
                         onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="01-01- 2019" max="12-31-2022"
+                    <input 
+                        type='date' min="01-01- 2019" max="12-31-2022"
+                        value={enteredDate}
                         onChange={dateChangeHandler} />
                 </div>
             </div>
