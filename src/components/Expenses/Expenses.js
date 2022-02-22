d0adcdb6546a3c96cebb7e3e7dc2a63e51f3bb73
David Castellar Martínez <castellardav@gmail.com>
2022-02-22 11:54:59 -0500

    Outputting conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,23 @@ const Exepenses = (props) => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
+    let expensesContent = <p>No expenses found.</p>
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense => (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
+
     return (
         <div>
             <Card className="expenses">
@@ -19,14 +36,7 @@ const Exepenses = (props) => {
                     onChangeFilter={filterChangeHandler} 
                 />
 
-                {props.items.map(expense => (
-                    <ExpenseItem
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                    ))
-                }
+                {expensesContent}
             </Card>
         </div>
     );
